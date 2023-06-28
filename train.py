3ce2f93a9850acf2d72bd09b771d05cde826e29e
ShaneKoNaung <shanekonaung@gmail.com>
2023-06-28 11:55:50 +0630

    implemented train_linear_sklearn

--- train.py
+++ train.py
@@ -1,19 +1,25 @@
-from sklearn.linear_model import LinearRegression
-from sklearn.linear_model import Lasso
-from sklearn.linear_model import Ridge
-
+from sklearn import linear_model
 from sklearn.metrics import mean_squared_error
 
+import scipy
 
+import mlflow
 
-def train_linear_regression(X_train, y_train, X_val, y_val):
+def train_linear_sklearn(model : linear_model ,X_train : scipy.sparse._csr.csr_matrix, 
+                         y_train : scipy.sparse._csr.csr_matrix, 
+                         X_val : scipy.sparse._csr.csr_matrix, 
+                         y_val : scipy.sparse._csr.csr_matrix) -> linear_model:
+    
+    with mlflow.start_run() as run:
 
-    lr = LinearRegression()
+        lr = model()
 
-    lr.fit(X_train, y_train)
+        lr.fit(X_train, y_train)
 
-    y_pred = lr.predict(X_val)
+        y_pred = lr.predict(X_val)
 
-    rmse = mean_squared_error(y_val, y_pred, squared=False)
+        rmse = mean_squared_error(y_val, y_pred, squared=False)
 
     return lr
+
+
